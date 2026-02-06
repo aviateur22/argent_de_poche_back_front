b4d5b577cf8a8f11aa37fd4f937bf9791592e4d5
cyrille <aviateur22@hotmail.fr>
2026-02-06 18:06:07 +0100

    First commit du projet

---
+++ src/environment/environment.moe.ts
@@ -0,0 +1,8 @@
+import { IEnvironment } from "./IEnvironment";
+
+export const environment: IEnvironment  = {
+  production: false,
+  name: 'moe',
+  api_base: 'http://52.47.100.124/argent-de-poche/api',
+  main_page_title: 'Argent de poche'
+}
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,8 @@
+import { IEnvironment } from "./IEnvironment";
+
+export const environment: IEnvironment  = {
+  production: true,
+  name: 'prod',
+  api_base: '/api',
+  main_page_title: 'Argent de poche'
+}
